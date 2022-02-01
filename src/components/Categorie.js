04ea675f27d1691517235f148ed721fbd2f00c52
BonkeG <bonkegcobo291195@gmail.com>
2022-02-01 14:25:13 +0200

    Add book info

---
+++ src/components/Categorie.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categorie = () => (
+  <>
+    <h1>Under Construction</h1>
+  </>
+);
+
+export default Categorie;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="theHeader">
+    <div className="HeaderRight">
+      <h1 className="title">Bookstore CMS</h1>
+      <ul className="nav">
+        <li>BOOKS</li>
+        <li>CATEGORIES</li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
