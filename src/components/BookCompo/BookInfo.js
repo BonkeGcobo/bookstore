f913e2af3bbbdcacad67456d937f23c7d68b2745
BonkeG <bonkegcobo291195@gmail.com>
2022-02-05 11:29:23 +0200

    Add and style progress bar

--- src/components/BookCompo/BookInfo.js
+++ src/components/BookCompo/BookInfo.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { removeBookFromApi } from '../../redux/books/books';
+import ProgressBar from './ProgressBar';
 
 const BookInfo = (book) => {
   const dispatch = useDispatch();
-  const { id, title, Categorie } = book;
+  const { id, title, category } = book;
 
   const removeHandler = () => {
     dispatch(removeBookFromApi(id));
   };
 
   return (
-    <div id={id}>
-      <h3>{title}</h3>
-      <p>{Categorie}</p>
-      <button type="button" onClick={removeHandler}>Delete</button>
+    <div id={id} className="bookCard">
+      <div className="BookDetails">
+        <p className="category-sec">{category}</p>
+        <h3>{title}</h3>
+
+        <div className="theButtons">
+          <button type="button" className="theBtns">Comments</button>
+          <button type="button" className="theBtns delete" onClick={removeHandler}>Delete</button>
+          <button type="button" className="theBtns">Edit</button>
+        </div>
+      </div>
+      <ProgressBar />
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   const links = [
     {
       id: 1,
       path: '/',
-      text: 'Books',
+      text: 'BOOKS',
     },
     {
       id: 2,
       path: 'Categories',
-      text: 'Categories',
+      text: 'CATEGORIES',
     },
   ];
 
   return (
     <header className="theHeader">
       <div className="HeaderRight">
-        <h1 className="title">Bookstore CMS</h1>
+        <h1 className="Apptitle">Bookstore CMS</h1>
         <ul className="nav">
           {links.map((link) => (
             <li key={link.id} className="link">
-              <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+              <NavLink to={link.path} activeClassName="active-link" className="tag" exact>{link.text}</NavLink>
             </li>
           ))}
         </ul>
+      </div>
+
+      <div className="user-icon">
+        <FontAwesomeIcon icon={faUser} color="#0290ff" />
       </div>
     </header>
   );
